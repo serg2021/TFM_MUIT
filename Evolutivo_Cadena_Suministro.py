d5d2671fe6847784b47aa2ea69f1d6adde5ea8d8
serg2021 <sergio.sastre@edu.uah.es>
2024-06-05 03:13:34 +0200

    Evolutivo Cadena Suministro Sin Modificar (Es Evolutivo Base)

--- Evolutivo_Cadena_Suministro.py
+++ Evolutivo_Cadena_Suministro.py
@@ -8,14 +8,15 @@
 import numpy as np
 from PyCROSL.CRO_SL import CRO_SL
 import random
+import math
 import matplotlib.pyplot as plt
 
 class EvolutiveClass:
-    def __init__(self, Num_Individuos=200, Num_Generaciones=10, Tam_Individuos=10, Num_Max = 5, Prob_Padres=0.5, Prob_Mutacion=0.02, Prob_Cruce=0.5):
-        self.Num_Individuos = Num_Individuos
+    def __init__(self, Num_Individuos=200, Num_Generaciones=10, Tam_Individuos=1, Num_Max = 10, Prob_Padres=0.5, Prob_Mutacion=0.02, Prob_Cruce=0.5):
+        self.Num_Individuos = Num_Individuos    #Número de posibles soluciones
         self.Num_Generaciones = Num_Generaciones
-        self.Tam_Individuos = Tam_Individuos
-        self.Num_Max = Num_Max
+        self.Tam_Individuos = Tam_Individuos    #Número de bases
+        self.Num_Max = Num_Max  #Número de SD
         self.Prob_Padres = Prob_Padres
         self.Num_Padres = round(self.Num_Individuos * self.Prob_Padres)
         self.Prob_Mutacion = Prob_Mutacion
@@ -30,28 +31,36 @@ class EvolutiveClass:
         print("Probabilidad de mutación: " + str(self.Prob_Mutacion))
         print("Probabilidad de cruce: " + str(self.Prob_Cruce))
     
-    def PoblacionInicial(self, Fil=None, Col=None, Num_Max=None):
+    def PoblacionInicial(self, Capacidades, Fil=None, Col=None, Num_Max=None):
         if Fil == None:
             Fil = self.Num_Individuos
         if Col == None:
             Col = self.Tam_Individuos
         if Num_Max == None:
             Num_Max = self.Num_Max
-        Pob_Ini = np.random.randint(0,Num_Max, size=(Fil,Col))
+        Pob_Ini = np.random.randint(0,Num_Max, size=(Fil,Col))  #Son los índices de los SD asignados a cada base
+        for i in range(Fil):    #Comprobamos todos los individuos y los reparamos si estuvieran mal
+            if(self.Comprobacion_Individuo(Pob_Ini[i], Capacidades)):
+                Pob_Ini[i] = self.Reparacion_Mayor_Menor(Pob_Ini[i], Capacidades)
         return Pob_Ini
 
     def Seleccion(self, poblacion_inicial, coste):
         index = np.argsort(coste)
         coste_ordenado = np.sort(coste)
-        poblacion_actual = poblacion_inicial[index,:]
-        poblacion_actual = poblacion_actual[0:self.Num_Padres,:]
+        poblacion_actual = poblacion_inicial[index,:]   #La población tendrá más soluciones que la inicial debido al cruce
+        poblacion_actual = poblacion_actual[0:self.Num_Individuos,:]    #Nos quedamos con los mejores individuos
         return poblacion_actual, coste_ordenado
 
-    def Cruce (self, poblacion, Num_Max = None):
+    def Cruce (self, poblacion, capacidades, Num_Max = None):
         if Num_Max == None:
             Num_Max = self.Num_Max
-        for i in range (self.Num_Individuos - self.Num_Padres):
-            Indice_Padres = random.sample(range(self.Num_Padres), 2)            # Se elige aleatoriamente el indice de los padres
+        #Indice_Seleccionado = []
+        Indices_Validos = list(np.arange(self.Num_Individuos))
+
+        for i in range(self.Num_Individuos - self.Num_Padres): #Bucle para generar HIJOS
+            Indice_Padres = random.sample(Indices_Validos, 2)
+            #Indice_Padres = random.sample([j for j in Indices_Validos if j not in Indice_Seleccionado], 2)            # Se elige aleatoriamente el indice de los padres
+            #Indice_Seleccionado.extend(Indice_Padres)   #Guardamos los índices elegidos para que no los vuelva a repetir en la siguiente iteración
             Padre1 = poblacion[Indice_Padres[0],:]                              # Se coge el padre 1
             Padre2 = poblacion[Indice_Padres[1],:]                              # Se coge el padre 2
             Hijo = np.copy(Padre1)                                              # El hijo va a ser una copia del padre 1
@@ -59,8 +68,8 @@ class EvolutiveClass:
             Hijo[np.where(vector==1)[0]] = Padre2[np.where(vector==1)[0]]       # Los genes seleccionados del padre 2 pasan al hijo
             if np.random.rand() < self.Prob_Mutacion:                           # Se comprueba si el hijo va a mutar
                 Hijo = self.Mutacion(Hijo, Num_Max)
-            # if sum(Hijo) != self.Max_BBU:                                       # Se comprueba si hay que reparar el hijo
-            #     Hijo = self.Reparacion (Hijo, Lista_Fibra)
+            if(self.Comprobacion_Individuo(Hijo, capacidades)):                    # Se comprueba si hay que reparar el hijo
+                 Hijo = self.Reparacion_Mayor_Menor(Hijo, capacidades)
             poblacion = np.insert(poblacion,self.Num_Padres+i,Hijo, axis = 0)   # Se añade a la población una vez que ha mutado y se ha reparado
         return poblacion
 
@@ -69,13 +78,69 @@ class EvolutiveClass:
         aux2 = np.random.randint(0,Num_Max)                                   # Se genera el número a modificar
         individuo[aux1] = aux2        
         return individuo
+    def Comprobacion_Individuo (self, individuo, capacidades):
+        for i in range(self.Num_Max):
+            indices_bases = [j for j, value in enumerate(individuo) if value == i]  #Obtenemos los indices de las bases asociadas a un SD "i"
+            comprobar_capacidades = capacidades[indices_bases]
+            if sum(comprobar_capacidades) > 200:    #Si la suma de las capacidades de una solucion para un supply depot es mayor que 200 -> REPARACION
+                return True
+            else:
+                return False
+    def Reparacion_Aleatorio (self, individuo, capacidades): #Sustituimos una base de un SD por otra (aleatoriamente) -> Hasta cumplir restricción
+        capacidades_sd = list(np.zeros(self.Num_Max))   #Capacidades de los SD
+        suma_capacidades = list(np.zeros(self.Num_Max)) #Suma de las capacidades de las bases
+        for i in range(self.Num_Max):
+            indices_bases_reparar = [j for j, value in enumerate(individuo) if value == i]
+            capacidades_sd_i = capacidades[indices_bases_reparar]
+            capacidades_sd[i] = capacidades_sd_i
+            suma_capacidades[i] = sum(capacidades_sd[i])    #Almacenamos todas las sumas de las capacidades en un array
+        Caps_SD_Superadas = [ind_cap for ind_cap, j in enumerate(suma_capacidades) if j > 200]  #Comprobamos en qué SD's se han superado la capacidad
+        for k in Caps_SD_Superadas:
+            while True:
+                indices_bases_SD = [j for j, value in enumerate(individuo) if value == k]    #Obtenemos índices de las bases cuya suma de caps supera el umbral
+                indices_resto_bases = [j for j, value in enumerate(individuo) if value != k]  # Obtenemos índices del resto de bases
+                indice_base_aleatoria_1 = random.choice(indices_bases_SD) #Elección aleatoria de la base del SD
 
-def Puntos_Sin_Repetir(num_points, offset=0.2):
+                indice_base_aleatoria_2 = random.choice(indices_resto_bases)  # Elección aleatoria de la base del resto de bases
+                individuo[indice_base_aleatoria_1], individuo[indice_base_aleatoria_2] = individuo[indice_base_aleatoria_2], individuo[indice_base_aleatoria_1] #Intercambio posiciones de las bases
+                indices_bases_reparadas = [j for j, value in enumerate(individuo) if value == k]    #Obtenemos índices de las bases cuya suma de caps supera el umbral
+                if sum(capacidades[indices_bases_reparadas]) > 200: #Si es más de 200, volvemos a hacer mismo código del while
+                    continue
+                else: #Si no, salimos del while y avanzamos en el for
+                    break
+        return individuo
+    def Reparacion_Mayor_Menor (self, individuo, capacidades): #Sustituimos una base de un SD por otra (aleatoriamente) -> Hasta cumplir restricción
+        capacidades_sd = list(np.zeros(self.Num_Max))   #Capacidades de los SD
+        suma_capacidades = list(np.zeros(self.Num_Max)) #Suma de las capacidades de las bases
+        for i in range(self.Num_Max):
+            indices_bases_reparar = [j for j, value in enumerate(individuo) if value == i]
+            capacidades_sd_i = capacidades[indices_bases_reparar]
+            capacidades_sd[i] = capacidades_sd_i
+            suma_capacidades[i] = sum(capacidades_sd[i])    #Almacenamos todas las sumas de las capacidades en un array
+        Caps_SD_Superadas = [ind_cap for ind_cap, j in enumerate(suma_capacidades) if j > 200]  #Comprobamos en qué SD's se han superado la capacidad
+        for k in Caps_SD_Superadas:
+            while True:
+                indices_bases_SD = [j for j, value in enumerate(individuo) if value == k]    #Obtenemos índices de las bases cuya suma de caps supera el umbral
+                indices_bases_SD = np.array(indices_bases_SD)
+                indices_resto_bases = [j for j, value in enumerate(individuo) if value != k]  # Obtenemos índices del resto de bases
+                capacidades_bases_SD_ordenados = list(np.argsort(capacidades[indices_bases_SD])[::-1])
+                indices_bases_SD_ordenados = indices_bases_SD[capacidades_bases_SD_ordenados]
+
+                indice_base_1 = indices_bases_SD_ordenados[0] #Elegimos la base del SD con mayor capacidad
+                indice_base_aleatoria_2 = random.choice([value for value in indices_resto_bases if capacidades[value] < capacidades[indice_base_1]])  # Elección aleatoria de la base del resto de bases
+                individuo[indice_base_1], individuo[indice_base_aleatoria_2] = individuo[indice_base_aleatoria_2], individuo[indice_base_1] #Intercambio posiciones de las bases
+                indices_bases_reparadas = [j for j, value in enumerate(individuo) if value == k]    #Obtenemos índices de las bases cuya suma de caps supera el umbral
+                if sum(capacidades[indices_bases_reparadas]) > 200: #Si es más de 200, volvemos a hacer mismo código del while
+                    continue
+                else: #Si no, salimos del while y avanzamos en el for
+                    break
+        return individuo
+
+def Puntos_Sin_Repetir(num_points, offset=0.5):
     points = set()  # Usamos un conjunto para evitar duplicados
     while len(points) < num_points:
-        latitud = np.random.uniform(low=-90.0, high=90.0)
-        longitud = np.random.uniform(low=-180.0, high=180.0)
-        points.add((latitud, longitud))  # Agregamos el punto al conjunto
+        latitud = np.random.uniform(low=0, high=180.0)
+        longitud = np.random.uniform(low=0, high=180.0)
         # Aplicar desplazamiento aleatorio para evitar superposiciones
         latitud_offset = np.random.uniform(low=-offset, high=offset)
         longitud_offset = np.random.uniform(low=-offset, high=offset)
@@ -83,33 +148,83 @@ def Puntos_Sin_Repetir(num_points, offset=0.2):
         points.add(point_with_offset)  # Agregamos el punto al conjunto
     return points
 
+def Distancia_Base_Supply_Depot_2D(base, supply):
+    x_supply, y_supply = zip(*supply)
+    x_base, y_base = zip(*base)
+    dist = []
+    for i in range(len(supply)):
+        dist_aux = []
+        for j in range(len(base)):
+            distancia = math.sqrt((x_base[j]-x_supply[i])**2 + (y_base[j]-y_supply[i])**2)
+            dist_aux.append(distancia)
+        dist.append(dist_aux)
+    return dist
+def Funcion_Fitness(distancias, poblacion):
+    lista_fitness = []
+    for i in range(len(poblacion)):    #Aplicamos la función fitness a cada solución
+        fitness = 0
+        for j in range(numero_bases):
+            SD = poblacion[i][j]    #Saco el SD asociado a una base de la población
+            fitness += distancias[SD][j]    #Calculo fitness buscando en la matriz de distancias la distancia asociada
+        fitness = fitness/numero_bases
+        lista_fitness.append(fitness)
+    return lista_fitness
+
 if __name__ == "__main__":
     # Definicion de los parámetros del genético
-    Num_Individuos = 200
-    Num_Generaciones = 20
-    Tam_Individuos = 100
+    Num_Individuos = 100
+    Num_Generaciones = 200
+    Tam_Individuos = 200
     Prob_Padres = 0.1
     Prob_Mutacion = 0.01
     Prob_Cruce = 0.5
 
+    Pob_Actual = []
+    Costes = []
     numero_bases = 200
-    bases = Puntos_Sin_Repetir(numero_bases)
+    numero_supply_depots = 10
+    capacidad_maxima = 20
+    puntos = list(Puntos_Sin_Repetir(numero_bases+numero_supply_depots))
+    supply_depots = puntos[-numero_supply_depots:]
+    bases = puntos[:numero_bases]
     longitudes_bases, latitudes_bases = zip(*bases)
-
-    numero_supply_depots = 20
-    supply_depots = Puntos_Sin_Repetir(numero_supply_depots)
+    capacidad_bases = np.random.randint(1, capacidad_maxima, size=(numero_bases))
+    indices_capacidad_bases = sorted(range(len(capacidad_bases)), key=lambda i: capacidad_bases[i])
     longitudes_supply_depots, latitudes_supply_depots = zip(*supply_depots)
+    capacidad_supply_depots = np.full(numero_supply_depots,200)
 
+    distancias_euclideas = Distancia_Base_Supply_Depot_2D(bases, supply_depots) #Obtenemos distancias de bases a supply depots
+    #distancias_euclideas_orden = []
+    #for j in indices_capacidad_bases:
+    #    distancias_euclideas_aux = []
+    #    for i in range(0, numero_supply_depots):
+    #        distancias_euclideas_aux.append(distancias_euclideas[i][j])
+    #    distancias_euclideas_orden.append(distancias_euclideas_aux) #Distancias de una base hacia los supply depot ordenadas según su capacidad
+    #distancias_capacidades_bases = list(zip(*[capacidad_bases[indices_capacidad_bases]], [distancias_euclideas_orden[l] for l in range(0, 200)])) #Ordenamos en una única lista
+
+    ### A CONTINUACIÓN, APLICAMOS EL ALGORITMO DESPUÉS DE OBTENER LOS COSTES Y DISTANCIAS
+    
+    Ev1 = EvolutiveClass(Num_Individuos, Num_Generaciones, Tam_Individuos,numero_supply_depots, Prob_Padres, Prob_Mutacion, Prob_Cruce)
+    #Ev1.ImprimirInformacion()
+    Pob_Inicial = Ev1.PoblacionInicial(capacidad_bases, 100, numero_bases, numero_supply_depots,)  #Poblacion inicial -> 100 posibles soluciones -> PADRES
+    for i in range(Num_Generaciones):
+        Pob_Actual = Ev1.Cruce(Pob_Inicial, capacidad_bases, numero_supply_depots)   #Aplicamos cruce en las soluciones
+        Fitness = Funcion_Fitness(distancias_euclideas, Pob_Actual)
+        Pob_Actual, Costes = Ev1.Seleccion(Pob_Actual,Fitness)
+    Sol_Final = Pob_Actual[0]   #La primera población será la que tenga menor coste
+    Coste_Final = Costes[0]
+    print("Solución final:")
+    for j in range(Tam_Individuos):
+        print("Base " + str(j) + "-> SD: " + str(Sol_Final[j]))
+    print("Coste de la solución: " + str(Coste_Final))
     # Graficar el mapa y los puntos
     plt.figure(figsize=(10, 6))
     plt.scatter(longitudes_bases, latitudes_bases, color='blue', label='Bases')
     plt.scatter(longitudes_supply_depots, latitudes_supply_depots, color='black', marker='p', label='Puntos de Suministro')
+    for k in range(Tam_Individuos):
+        plt.plot([longitudes_bases[k],longitudes_supply_depots[Sol_Final[k]]], [latitudes_bases[k], latitudes_supply_depots[Sol_Final[k]]],color='red')
     plt.xlabel('Longitud')
     plt.ylabel('Latitud')
     plt.title('Mapa con Puntos Aleatorios')
     plt.legend(bbox_to_anchor=(0, 0), loc='upper left')
     plt.show()
-    
-    #Ev1 = EvolutiveClass(Num_Individuos, Num_Generaciones, Tam_Individuos, Prob_Padres, Prob_Mutacion, Prob_Cruce)
-    #Ev1.ImprimirInformacion()
-    #Pob = Ev1.PoblacionInicial(None, 100, 5)
